8c2d418dcda9ee59054fd65df1dc4d2202a86b2b
DhruvilSoni <dhruvilsoni1703@gmail.com>
2021-03-08 22:28:57 +0530

    Update DivideIntegers.java

--- DivideIntegers.java
+++ DivideIntegers.java
@@ -30,59 +30,59 @@ public class DivideIntegers {
 	{
 		if(a <= Integer.MAX_VALUE && b==1)
 		{
-			return a;
+		      return a;
 		}
 		if(a > Integer.MAX_VALUE && b==1)
 		{
-			return Integer.MAX_VALUE;
+		      return Integer.MAX_VALUE;
 		}
 		if(b == -1)
 		{
 			if(a == Integer.MIN_VALUE)
 			{
-				return Integer.MAX_VALUE;
+			      return Integer.MAX_VALUE;
 			}
 			else
 			{
-				return -a;
+			      return -a;
 			}	
 		}
 		if(a == b)
 		{
-			return 1;
+		      return 1;
 		}
 		int sign  = 1;
 		if((a < 0 && b > 0) || (a > 0 && b < 0))
 		{
-			sign = -1;
+		     sign = -1;
 		}
 		long A = Math.abs((long)a);
 		long B = Math.abs((long)b);
 
 		int count = 0;
 		while(A != 0)
-        {
-            if(A >= B)
-            {
-                A = A - B;
-                count++;
-                if(count >= Integer.MAX_VALUE)
-                {
-                	return Integer.MAX_VALUE;
+        	{
+            	     if(A >= B)
+            	     {
+                           A = A - B;
+                           count++;
+                           if(count >= Integer.MAX_VALUE)
+                           {
+                                 return Integer.MAX_VALUE;
+                           }
+                     }
+                     else
+                     {
+                           break;
+                     }
                 }
-            }
-            else
-            {
-                break;
-            }
-        }
 		if(sign == -1)
 		{
-			return -count;
+		      return -count;
 		}
 		else
 		{
-			return count;
+		      return count;
 		}
 	}
 }
